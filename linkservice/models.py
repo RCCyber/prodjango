a5cc9f720b101abf8cdaf8c2f8aaf305ef6ddbfe
RCCyber <43203558+RCCyber@users.noreply.github.com>
2018-09-25 16:19:13 +0300

    Update models.py

--- linkservice/models.py
+++ linkservice/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
+class LinkBase(models.Model):
+    long = models.CharField(max_length=500)
+    short = models.CharField(max_length=30)
+    date = models.DateField
+
+    def __str__(self):
+        return self.short
+
 # Create your models here.
