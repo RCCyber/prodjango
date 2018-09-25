c5b96b4fefd66b1ee05934e05d2c5b74c05e9e71
RCCyber <43203558+RCCyber@users.noreply.github.com>
2018-09-25 16:18:32 +0300

    Update urls.py

--- linkservice/urls.py
+++ linkservice/urls.py
@@ -1,8 +1,12 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
+from django.views.generic import ListView, DetailView
+from linkservice.models import LinkBase
 from . import views
 
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^out/', views.out, name='out'),
+#    url(r'^request/$', views.in_request, name='in_request'),
+    url(r'^in_request/$', ListView.as_view(queryset=LinkBase.objects.all(), template_name="linkservice/out.html")),
+    url(r'^(?P<pk>\d+)$', DetailView.as_view(model= LinkBase, template_name="in_request/out.html"))
 ]
