0de7403b7bec04b1ad3febf8ed8a74de8f8e67d1
RCCyber <43203558+RCCyber@users.noreply.github.com>
2018-09-25 16:18:54 +0300

    Update views.py

--- linkservice/views.py
+++ linkservice/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return render(request, 'linkservice/linkservice.html', )
+
+
+def in_request(request):
+    return render(request, 'linkservice/out.html', {'values': ['http://localhost:8000/5Gh83Nb', ' take it']})
